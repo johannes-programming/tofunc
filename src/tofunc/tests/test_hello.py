6d08b27e483309a68e01a4c3c6edca652626cc0a
Johannes <johannes-programming@mailfence.com>
2025-08-27 17:01:53 +0200

    Version 1.0.1.dev0

--- src/tofunc/tests/test_hello.py
+++ src/tofunc/tests/test_hello.py
@@ -1,22 +1,24 @@
 import functools
+import types
 import unittest
+from typing import *
 
 from tofunc import tofunc
 
 
 class TestHello(unittest.TestCase):
-    def test_hello(self):
-        def join(self, b="beta", c="gamma"):
+    def test_hello(self: Self) -> None:
+        def join(self: Self, b: Any = "beta", c: Any = "gamma") -> str:
             return "%s %s %s" % (self, b, c)
 
         class Foo: ...
 
-        hello = functools.partial(join, c="hello")
+        hello: functools.partial = functools.partial(join, c="hello")
         Foo.greet = hello
         self.assertEqual(Foo().greet("Alice"), "Alice beta hello")
-        hello = tofunc(hello)
+        hello: types.FunctionType = tofunc(hello)
         Foo.greet = hello
-        text = Foo().greet("Bob")
+        text: str = Foo().greet("Bob")
         self.assertTrue(text.endswith("Bob hello"))
         self.assertTrue("Foo" in text)
 
